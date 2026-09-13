6a6b9f541b7b0a99595e5b6203693ecffa594c99
nandaniyajinal1-bit <nandaniyajinal1@gmail.com>
2026-09-13 20:18:44 +0530

    Remove commented-out code in StudentDemo.cs

--- StudentDemo.cs
+++ StudentDemo.cs
@@ -1,4 +1,4 @@
-﻿/*using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -90,4 +90,4 @@ namespace ASP_NET
             Console.ReadLine();
         }
     }
-}*/
+}
