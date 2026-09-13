59cc48a91e50fe1551527531e09c1761187746b5
nandaniyajinal1-bit <nandaniyajinal1@gmail.com>
2026-09-13 20:19:25 +0530

    Remove unused code comments
    Removed commented-out code block from StudentInformation.cs.

--- StudentInformation.cs
+++ StudentInformation.cs
@@ -1,4 +1,4 @@
-﻿/*using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -63,4 +63,4 @@ namespace ASP_NET
             Console.ReadLine();
         }
     }
-}*/
+}
