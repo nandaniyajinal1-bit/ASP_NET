b3776008aa8c168e4a52902a0874bbc20bb309ce
nandaniyajinal1-bit <nandaniyajinal1@gmail.com>
2026-09-13 20:09:11 +0530

    Remove commented-out code in Employee.cs

--- Employee.cs
+++ Employee.cs
@@ -1,4 +1,4 @@
-﻿/*using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -94,6 +94,6 @@ namespace ASP_NET
                 Console.ReadLine();
             }
         }
-}*/
+}
 
 
